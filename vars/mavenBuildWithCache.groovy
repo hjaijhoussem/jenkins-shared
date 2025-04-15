554007d416d9a97d2ae63877b8c5dab84f241321
hjaijhoussem <hjaijhoussem@gmail.com>
2025-04-15 15:49:44 +0100

    added maven build function

---
+++ vars/mavenBuildWithCache.groovy
@@ -0,0 +1,24 @@
+def call(Map config = [:]) {
+    // Default parameter values
+    def dirPath = config.dirPath ?: '.'
+    def mavenSettingsId = config.mavenSettingsId ?: 'mvn-nexus-jar'
+    def maxCacheSize = config.maxCacheSize ?: 1000
+    def cachePath = config.cachePath ?: '/root/.m2/repository'
+    
+    dir(path: dirPath) {
+        configFileProvider([configFile(fileId: mavenSettingsId, variable: 'MAVEN_SETTINGS')]) {
+            cache(
+                maxCacheSize: maxCacheSize,
+                caches: [
+                    arbitraryFileCache(
+                        path: cachePath,
+                        cacheValidityDecidingFile: 'pom.xml',
+                        includes: '**/*'
+                    )
+                ]
+            ) {
+                sh sh "mvn clean install -DskipTests -s $MAVEN_SETTINGS"
+            }
+        }
+    }
+}
