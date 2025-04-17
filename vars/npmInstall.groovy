425e5c4c7ff35e02776f88209b3eaf8ed73a9623
hjaiej_houssem <85972064+hjaijhoussem@users.noreply.github.com>
2025-04-17 04:36:24 -0700

    Create npmInstall.groovy

---
+++ vars/npmInstall.groovy
@@ -0,0 +1,12 @@
+def call (){
+  cache(maxCacheSize: 550, caches: [
+      arbitraryFileCache(
+          cacheName: 'npm-dependency-cache',
+          cacheValidityDecidingFile: 'package-lock.json',
+          includes: '**/*',
+          path: 'node_modules'
+      )
+  ]) {
+      sh 'npm install --force --no-audit'
+  }
+}
