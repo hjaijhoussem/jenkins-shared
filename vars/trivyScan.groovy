89b7fe6bcb7025d9755b49a2e4dd9e24ba0aba86
hjaiej_houssem <85972064+hjaijhoussem@users.noreply.github.com>
2025-04-17 04:29:46 -0700

    Create trivyScan.groovy

---
+++ vars/trivyScan.groovy
@@ -0,0 +1,38 @@
+def vulnerability(Map config) {
+    // Load the script
+    loadScript('trivy.sh')
+    
+    // Execute with parameters
+    sh "./trivy.sh ${config.imageName} ${config.severity} ${config.exitCode}"
+}
+
+def reportsConverter() {
+    sh '''
+        # Convert LOW,MEDIUM results to JUnit XML and HTML
+
+        trivy convert --format template --template "@/usr/local/share/trivy/templates/junit.tpl" \
+        --input trivy-image-MEDUIM-results.json --output trivy-junit-MEDUIM-report.xml
+        
+        trivy convert --format template --template "@/usr/local/share/trivy/templates/html.tpl" \
+        --input trivy-image-MEDUIM-results.json --output trivy-MEDUIM-report.html
+        
+        # Convert HIGH,CRITICAL results to XML and HTML
+
+        trivy convert --format template --template "@/usr/local/share/trivy/templates/junit.tpl" \
+        --input trivy-image-CRITICAL-results.json --output trivy-junit-CRITICAL-report.xml
+
+        trivy convert --format template --template "@/usr/local/share/trivy/templates/html.tpl" \
+        --input trivy-image-CRITICAL-results.json --output trivy-CRITICAL-report.html
+    '''
+}
+
+/***
+Usage:
+- Call method:
+  trivyScan(
+    imageName: 'my-app:latest',
+    severity: 'HIGH',
+    exitCode: 1
+  )
+
+***/
