7d99ba2dc6ea982eb0cc381801e90a8008c172ac
hjaijhoussem <hjaijhoussem@gmail.com>
2025-04-19 23:06:55 +0100

    added trivy scan with custom tool

---
+++ vars/trivy.groovy
@@ -0,0 +1,29 @@
+def scan(Map config = [:]) {
+
+    def nexusUrl = config.nexusUrl // e.g. 'nexus.example.com
+    def nexusRepository = config.nexusRepository // e.g. 'docker-repo'
+    def dockerImageName = config.dockerImageName // e.g. 'my-docker-image'
+    def dockerImageVersion = config.dockerImageVersion
+
+    def trivyHome = tool name: 'trivy-0.58.1', type: 'com.cloudbees.jenkins.plugins.customtools.CustomTool'
+
+    sh 'curl -o html.tpl https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl'
+
+    sh """
+        ${trivyHome}/trivy image --format template --template "@html.tpl" \\
+            -o trivy-image-MEDIUM-results.html \\
+            --severity LOW,MEDIUM,HIGH \\
+            --exit-code 0 \\
+            --quiet \\
+            ${nexusUrl}/repository/${nexusRepository}/${dockerImageName}:${dockerImageVersion}
+    """
+
+    sh """
+        ${trivyHome}/trivy image --format template --template "@html.tpl" \\
+            -o trivy-image-CRITICAL-results.html \\
+            --severity CRITICAL \\
+            --exit-code 1 \\
+            --quiet \\
+            ${nexusUrl}/repository/${nexusRepository}/${dockerImageName}:${dockerImageVersion}
+    """
+}
